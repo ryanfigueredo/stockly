6140ecb42f0ec61e6e8896153ca83fc9a65ed8bf
Ryan <ryan.figueredo059@gmail.com>
2024-10-26 17:32:43 -0300

    feat: initial setups, class tailwind css

---
+++ app/_components/sidebar.tsx
@@ -0,0 +1,18 @@
+const Sidebar = () => {
+  return (
+    <div className="w-64 bg-white">
+      {/* Imagem */}
+      <div className="px-8 py-6">
+        <h1 className="font-bold text-2xl">Stockly</h1>
+      </div>
+      {/* Botões */}
+      <div className="flex flex-col gap-2 p-2">
+        <button className="px-6 py-3">Dashboard</button>
+        <button className="px-6 py-3">Produtos</button>
+        <button className="px-6 py-3">Vendas</button>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
