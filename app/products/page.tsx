6d0c7258a10a6cdcabab975809639627a2c7982b
Ryan <ryan.figueredo059@gmail.com>
2024-10-27 20:43:37 -0300

    feat: refactore route handlers

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,10 +2,10 @@ import { PlusIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import { productTableColumns } from "./_components/table-columns";
 import { DataTable } from "../_components/ui/data-table";
-import { getProducts } from "../_data-acess/product/get-products";
 
 const ProductsPage = async () => {
-  const products = await getProducts();
+  const response = await fetch("http://localhost:3000/api/products");
+  const products = response.json();
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
       <div className="flex w-full items-center justify-between">
@@ -19,7 +19,10 @@ const ProductsPage = async () => {
           <PlusIcon size={20} /> Novo Produto
         </Button>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.stringify(JSON.stringify(products))}
+      />
     </div>
   );
 };
