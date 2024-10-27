d2126b3e0bbfee251ea5eba2ef898ea0f63aeda2
Ryan <ryan.figueredo059@gmail.com>
2024-10-27 20:57:10 -0300

    feat: add dynamic route handlers

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/app/_lib/prisma";
+import { NextApiRequest } from "next";
+
+export async function GET(
+  request: NextApiRequest,
+  { params }: { params: { id: string } },
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("teste");
+  console.log(query);
+  const productId = params.id;
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) {
+    return Response.json({ message: "Product not found" }, { status: 404 });
+  }
+  return Response.json(product, { status: 200 });
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  await db.product.delete({
+    where: {
+      id: params.id,
+    },
+  });
+  return Response.json({}, { status: 200 });
+}
