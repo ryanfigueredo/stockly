9b10ecf331ea68386cdf1eb038516f87931fadbf
Ryan <ryan.figueredo059@gmail.com>
2024-10-27 19:02:02 -0300

    feat: add data-acess upgrade security next.js and server-only

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,11 @@
 import { PlusIcon } from "lucide-react";
-import { db } from "../_lib/prisma";
 import { Button } from "../_components/ui/button";
 import { productTableColumns } from "./_components/table-columns";
 import { DataTable } from "../_components/ui/data-table";
+import { getProducts } from "../_data-acess/product/get-products";
 
 const ProductsPage = async () => {
-  const products = await db.product.findMany({});
+  const products = await getProducts();
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
       <div className="flex w-full items-center justify-between">
