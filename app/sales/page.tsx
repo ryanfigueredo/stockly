b1d55bbb61f436b1158604df7bc7938589bb2ea9
Ryan <ryan.figueredo059@gmail.com>
2024-11-19 18:13:47 -0300

    feat: implement create sale functionality with validation and UI integration

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,9 +1,6 @@
-import { FormProvider } from "react-hook-form";
-import { Button } from "../_components/ui/button";
 import { ComboboxOption } from "../_components/ui/combobox";
-import { Sheet, SheetTrigger } from "../_components/ui/sheet";
 import { getProducts } from "../_data-acess/product/get-products";
-import UpsertSheetContent from "./_components/upsert-sheet-content";
+import CreateSaleButton from "./_components/create-sale-button";
 
 const SalesPage = async () => {
   const products = await getProducts();
@@ -18,18 +15,11 @@ const SalesPage = async () => {
           <span className="text-xs font-semibold text-slate-500">
             Gestão de Vendas
           </span>
-          <h2 className="text-ls font-semibold">Produtos</h2>
+          <h2 className="text-xl font-semibold">Vendas</h2>
         </div>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button>Nova Venda</Button>
-          </SheetTrigger>
-          <UpsertSheetContent
-            products={products}
-            productOptions={productOptions}
-          />
-        </Sheet>
+        <CreateSaleButton products={products} productOptions={productOptions} />
       </div>
+      {/* <DataTable columns={productTableColumns} data={products} /> */}
     </div>
   );
 };
