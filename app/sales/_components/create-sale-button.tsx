652fd72183c1e658b1c3c72f0afd81731f4477bc
Ryan <ryan.figueredo059@gmail.com>
2024-11-23 01:46:22 -0300

    feat: enhance create sale button with icon and improved layout

--- app/sales/_components/create-sale-button.tsx
+++ app/sales/_components/create-sale-button.tsx
@@ -5,6 +5,7 @@ import UpsertSheetContent from "./upsert-sheet-content";
 import { Product } from "@prisma/client";
 import { ComboboxOption } from "@/app/_components/ui/combobox";
 import { useState } from "react";
+import { PlusIcon } from "lucide-react";
 interface CreateSaleButtonProps {
   products: Product[];
   productOptions: ComboboxOption[];
@@ -14,7 +15,9 @@ const CreateSaleButton = (props: CreateSaleButtonProps) => {
   return (
     <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
       <SheetTrigger asChild>
-        <Button>Nova Venda</Button>
+        <Button className="gap-2">
+          <PlusIcon size={20} /> Nova Venda
+        </Button>
       </SheetTrigger>
       <UpsertSheetContent setSheetIsOpen={setSheetIsOpen} {...props} />
     </Sheet>
