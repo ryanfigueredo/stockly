a52c982bbccf7539bb46722c5370861a9eff9c36
Ryan <ryan.figueredo059@gmail.com>
2024-11-26 16:13:58 -0300

    feat: refactor page layouts to use a consistent header component with title and subtitle

---
+++ app/_components/header.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import { cn } from "../_lib/utils";
+
+export const HeaderTitle = ({ children }: { children: ReactNode }) => {
+  return <h2 className="text-xl font-semibold">{children}</h2>;
+};
+
+export const HeaderSubtitle = ({ children }: { children: ReactNode }) => {
+  return <h2 className="text-xl font-semibold">{children}</h2>;
+};
+
+export const HeaderLeft = ({ children }: { children: ReactNode }) => {
+  return <div className="space-y-1">{children}</div>;
+};
+
+export const HeaderRight = ({ children }: { children: ReactNode }) => {
+  return <div className="space-y-1">{children}</div>;
+};
+
+const Header = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex w-full items-center justify-between", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
