293485cafa9838cd838c78c2d69984d28532a652
Ryan Figueredo <ryan.figueredo059@gmail.com>
2024-11-27 21:42:34 -0300

    fix: update import path for DayTotalRevenue in revenue chart component

--- app/(dashboard)/_components/revenue-chart.tsx
+++ app/(dashboard)/_components/revenue-chart.tsx
@@ -7,7 +7,7 @@ import {
   ChartTooltipContent,
 } from "@/app/_components/ui/chart";
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
-import { DayTotalRevenue } from "@/app/_data-access/dashboard/getDashboard";
+import { DayTotalRevenue } from "../../_data-access/dashboard/getDashboard";
 
 const chartConfig = {
   totalRevenue: {
